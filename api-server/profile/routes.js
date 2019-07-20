54ed9e0ee29e8a5517ef0fa19c3108a51c1d0929
Prasamsha15 <pari_devil6915@yahoo.com>
2019-07-20 13:35:28 +0200

    updated version

--- api-server/profile/routes.js
+++ api-server/profile/routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const controller = require("./controller").default
+const controller = require("./controller")
 
 // routes =>
 
